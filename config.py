4fabbf3ba542aa6cc1d249df3b04a8eaf6dbaa93
Eren Yeager <154471897+SquirtleRivals09@users.noreply.github.com>
2024-08-08 15:40:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '6744296768:AAGOsNzdqVuQ36Z_gBexGDRLSGlAxaI19Hc' # Get Your Telegram Bot Token from @BotFather
+TOKEN = '7381512423:AAHId3hjjyI8_kXCfzyU2GgpYtr_DLEN96k' # Get Your Telegram Bot Token from @BotFather
 OWNER_ID = '1737646273' # Add Here your telegram id 
